b9c8e8e3cf8ec7b312c7f567c493a90414987898
Thomas McKee <caffeine.storm@gmail.com>
2025-06-02 11:38:28 -0400

    use 'set -e' to make sure we don't think things are okay when they are not

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# If a subcommand fails, bail out of the script.
+set -e
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y \
@@ -15,16 +18,20 @@ apt-get install -y \
 	xvfb \
 	;
 
+set +e
 go version &> /dev/null
-if [ $? -ne 0 ]; then
-	echo "3333f6ea53afa971e9078895eaa4ac7204a8c6b5c68c10e6bc9a33e8e391bdd8  go1.24.3.linux-amd64.tar.gz" > go1.24.3.sha256sum
-	curl 'https://go.dev/dl/go1.24.3.linux-amd64.tar.gz' --location --output go1.24.3.linux-amd64.tar.gz
-	sha256sum --check go1.24.3.sha256sum || {
-		echo 1>&2 "checksum failed -- aborting"
-		exit 1
-	};
-	tar --directory /usr/local --extract --file go1.24.3.linux-amd64.tar.gz
-	rm -f go1.24.3.linux-amd64.tar.gz
-	export PATH="$PATH:/usr/local/go/bin"
-	echo -e '\nexport PATH="$PATH:/usr/local/go/bin"' >> /etc/bash.bashrc
+if [ $? -eq 0 ]; then
+	exit 0
 fi
+set -e
+
+echo "3333f6ea53afa971e9078895eaa4ac7204a8c6b5c68c10e6bc9a33e8e391bdd8  go1.24.3.linux-amd64.tar.gz" > go1.24.3.sha256sum
+curl 'https://go.dev/dl/go1.24.3.linux-amd64.tar.gz' --location --output go1.24.3.linux-amd64.tar.gz
+sha256sum --check go1.24.3.sha256sum || {
+	echo 1>&2 "checksum failed -- aborting"
+	exit 1
+};
+tar --directory /usr/local --extract --file go1.24.3.linux-amd64.tar.gz
+rm -f go1.24.3.linux-amd64.tar.gz
+export PATH="$PATH:/usr/local/go/bin"
+echo -e '\nexport PATH="$PATH:/usr/local/go/bin"' >> /etc/bash.bashrc
